4c454b4a1c347b3b503ca9fb39e675186b76c5a8
kami-christella <kamichristella@gmail.com>
2025-03-30 14:05:34 +0200

    improved  admin dashboard cards design

--- src/Dashboard/AdminPage.jsx
+++ src/Dashboard/AdminPage.jsx
@@ -8,7 +8,7 @@ function AdminPage(){
   <div className="col-sm-4">
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
+        {/* <h5 className="card-title">Special title treatment</h5> */}
         <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
         {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
       </div>
@@ -17,7 +17,7 @@ function AdminPage(){
   <div className="col-sm-4">
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
+        {/* <h5 className="card-title">Special title treatment</h5> */}
         <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
         {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
       </div>
@@ -27,7 +27,7 @@ function AdminPage(){
 <div className="col-sm-4">
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
+        {/* <h5 className="card-title">Special title treatment</h5> */}
         <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
         {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
       </div>
